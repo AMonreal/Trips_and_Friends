bb15adbfc35d5228077a1f34bb291744bd87662a
Ariane Monreal <arianemonreal@hotmail.fr>
2018-01-09 11:39:11 +0100

    signup&login

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-        sessions: 'users/sessions'
-      }
-root to: 'pages#home'
-
-get 'users/sign_in', to: 'devise/sessions#new'
-get '/login', to: 'sessions#new'
-post '/login', to: 'sessions#create'
-delete '/logout', to: 'sessions#destroy'
-resources :users
+ devise_scope :user do
+    get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
+    get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
+  end
 
 
-    end
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 # get    '/users/sign_in',  to: 'devise/sessions#new'
