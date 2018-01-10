841fe1017e3e5f54cea5da48af8d3b1dc9ecd513
Ariane Monreal <arianemonreal@hotmail.fr>
2018-01-10 14:00:37 +0100

    déplacement home/application

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home, raise: false
+
+  def home
+  end
+
   def show
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
- devise_scope :user do
-    get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
-    get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
-  end
-
 root to: 'pages#home'
+devise_for :users
+
 end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-# get    '/users/sign_in',  to: 'devise/sessions#new'
-# get    '/login',   to: 'sessions#new'
-# post   '/login',   to: 'sessions#create'
-# delete '/logout',  to: 'sessions#destroy'
-# resources :users
+
