f19507a9d63811018bd8d4715bec7ed602d24d85
Ariane Monreal <arianemonreal@hotmail.fr>
2018-01-11 15:41:56 +0100

    logos

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,9 @@ module TripsAndFriends
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    # config.assets.precompile += %w(.svg)
+    config.assets.paths << Rails.root.join("app/assets/images")
+    config.assets.precompile << "images/backpack.svg"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
