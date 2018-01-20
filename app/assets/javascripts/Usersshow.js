85bfc620056a8aa7147a74b5abf64f621d590869
Ariane Monreal <arianemonreal@hotmail.fr>
2018-01-20 15:05:04 +0100

    incompréhension js

--- app/assets/javascripts/Usersshow.js
+++ app/assets/javascripts/Usersshow.js
@@ -1,20 +1,20 @@
-// new Shubox("#avatar", {
-//   // image transform queue
-//   transformName: "avatar-demo",
-//   // do not use built-in previewsContainer
-//   previewsContainer: false,
-//   // on success - insert the image w/a flip-in
-//   // animate.css classname
-//   success: function(file) {
-//     img = new Image();
-//     img.onload = function() {
-//       $(".avatar").append(
-//         '<img src="' + file.s3url + '" class="animated flipInY" />'
-//       );
-//     };
-//     img.src = file.s3url;
-//   }
-// });
+new Shubox("#avatar", {
+  // image transform queue
+  transformName: "avatar-demo",
+  // do not use built-in previewsContainer
+  previewsContainer: false,
+  // on success - insert the image w/a flip-in
+  // animate.css classname
+  success: function(file) {
+    img = new Image();
+    img.onload = function() {
+      $(".avatar").append(
+        '<img src="' + file.s3url + '" class="animated flipInY" />'
+      );
+    };
+    img.src = file.s3url;
+  }
+});
 
 
 // var handler = Gmaps.build('Google');
