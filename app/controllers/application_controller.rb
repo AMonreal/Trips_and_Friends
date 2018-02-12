725b584c14fa074c3be507ae79af73f7dd64fb5d
Ariane Monreal <arianemonreal@hotmail.fr>
2018-02-12 13:58:52 +0100

    la login page a dipsaru, on ne sait pas pourquoi...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :set_time_zone, if: :user_signed_in?
+  # before_action :set_time_zone, if: :user_signed_in?
 
-  private
+  # private
 
-    def set_time_zone
-      Time.zone = current_user.time_zone
-    end
+  #   def set_time_zone
+  #     Time.zone = current_user.time_zone
+  #   end
 
 end
